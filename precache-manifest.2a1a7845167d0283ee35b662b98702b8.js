ca820fc6f0c5d052601427070f8bb74d61632901
Jatin-patel123 <ajatinpatel@gmail.com>
2023-11-17 22:57:24 +0530

    Updates

---
+++ precache-manifest.2a1a7845167d0283ee35b662b98702b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e17f026704a49f93e9a",
+    "url": "/News-react-app/static/css/main.a9cd6804.chunk.css"
+  },
+  {
+    "revision": "5e17f026704a49f93e9a",
+    "url": "/News-react-app/static/js/main.5e17f026.chunk.js"
+  },
+  {
+    "revision": "6cc7b2a4696b63fa60b5",
+    "url": "/News-react-app/static/js/1.6cc7b2a4.chunk.js"
+  },
+  {
+    "revision": "3c6a5fbfb7fb1647dfb1",
+    "url": "/News-react-app/static/js/runtime~main.3c6a5fbf.js"
+  },
+  {
+    "revision": "3a99566bbc9bff2b4a5a73491df0c762",
+    "url": "/News-react-app/index.html"
+  }
+];
